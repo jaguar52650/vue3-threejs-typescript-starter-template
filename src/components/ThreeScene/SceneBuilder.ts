fc0a688e83e68acb37b3de0e90d0df967d09d8db
Олег Шмыгля <shmyglia@gmail.com>
2024-12-05 17:54:25 +0000

    day 2

--- src/components/ThreeScene/SceneBuilder.ts
+++ src/components/ThreeScene/SceneBuilder.ts
@@ -3,6 +3,7 @@ import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 import { toRaw } from "vue";
 //https://holomorphicguy.hashnode.dev/using-threejs-with-vue3-and-typescript
+import theCategories from "./categories.json";
 
 export default class SceneBuilder {
   root: HTMLElement;
@@ -48,7 +49,7 @@ export default class SceneBuilder {
 
     // GRID
     const gridHelper = new THREE.GridHelper(500, 50);
-    this.scene.add(gridHelper);
+    //this.scene.add(gridHelper);
 
     //AXES
     const axesHelper = new THREE.AxesHelper(5);
@@ -80,16 +81,30 @@ export default class SceneBuilder {
     cube.position.set(0.5, 1, -0.5);
     //this.scene.add(cube);
 
-    this.addProduct(
-      "https://imgcdn.zarina.ru/upload/images/42250/thumb/900_9999/4225070571_153_1.webp?t=1712655854",
-      "https://imgcdn.zarina.ru/upload/images/42250/thumb/900_9999/4225070571_153_4.webp?t=1712655857",
-      0
-    );
-    this.addProduct(
-      "https://imgcdn.zarina.ru/upload/images/44221/thumb/900_9999/4422142332_33_1.webp?t=1732025102",
-      "https://imgcdn.zarina.ru/upload/images/44221/thumb/900_9999/4422142332_33_5.webp?t=1732025102",
-      1
-    );
+    // this.addProduct(
+    //   "https://imgcdn.zarina.ru/upload/images/42250/thumb/900_9999/4225070571_153_1.webp?t=1712655854",
+    //   "https://imgcdn.zarina.ru/upload/images/42250/thumb/900_9999/4225070571_153_4.webp?t=1712655857",
+    //   0
+    // );
+    // this.addProduct(
+    //   "https://imgcdn.zarina.ru/upload/images/44221/thumb/900_9999/4422142332_33_1.webp?t=1732025102",
+    //   "https://imgcdn.zarina.ru/upload/images/44221/thumb/900_9999/4422142332_33_5.webp?t=1732025102",
+    //   1
+    // );
+
+    // Create a GridHelper
+    let gridHelper2 = new THREE.GridHelper(20, 20, 0x555555, 0x555555, 10, 10);
+
+    // Add objects to the grid
+    for (let i = 0; i < 100; i++) {
+      const object = new THREE.Mesh(
+        new THREE.BoxGeometry(1, 1, 1),
+        new THREE.MeshBasicMaterial({ color: 0xff0000 })
+      );
+      //gridHelper2.addObject(object);
+    }
+    this.scene.add(gridHelper2);
+    this.drawFloor(701);
   }
   addProduct = (front, back, index) => {
     const loadManager = new THREE.LoadingManager();
@@ -114,6 +129,33 @@ export default class SceneBuilder {
       cube.position.set(0.5 + index * width * 1.5, 1, -0.5);
       this.scene.add(cube);
     };
+  };
+
+  drawFloor = (category) => {
+    let level = this.getLevel(category, theCategories);
+    console.log(level);
+    let index = 0;
+    for (let i in level.subs) {
+      if (level.subs[i].picture_bar) {
+        this.addProduct(
+          level.subs[i].picture_bar_img,
+          level.subs[i].picture_bar_img,
+          index++
+        );
+      }
+    }
+  };
+  getLevel = (categoryId, levels) => {
+    for (let i in levels) {
+      if (levels[i].id == categoryId) {
+        return levels[i];
+      } else if (levels[i].subs.length) {
+        let res = this.getLevel(categoryId, levels[i].subs);
+        if (res) {
+          return res;
+        }
+      }
+    }
   };
 
   render = () => {
